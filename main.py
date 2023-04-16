8209274951e174ce5407666d7bf1dbdbf16872a0
idris <idrisbahce26@gmail.com>
2023-04-16 13:12:24 +0300

    motivation tool

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import datetime as dt
+import smtplib
+import random
+
+now = dt.datetime.today()
+
+MY_EMAIL = "*********@****.com"#Write here the email you want to send from
+MY_PASSWORD = "***************"#Write here the password of your email.
+# It is better to take specific password for this resason
+
+with open("quotes.txt", "r") as quotes:
+    lines = quotes.readlines()
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+    if now.strftime("%A").lower() == "sunday" or now.strftime("%A").lower() == "tuesday" or\
+            now.strftime("%A").lower() == "friday":
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="idrisbahce26@gmail.com",
+            msg=f"Subject:MOTIVATION!\n\n{random.choice(lines)}"
+        )
